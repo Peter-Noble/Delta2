8a3c210484c8dc97705fdad9345792777f6bcc59
Peter-Noble <peter@noblesque.org.uk>
2022-09-27 16:23:52 +0100

    Sub step when errors

--- src/strategies/broad_phase/broad_phase_embree_cluster.cpp
+++ src/strategies/broad_phase/broad_phase_embree_cluster.cpp
@@ -14,35 +14,111 @@ BroadPhaseEmbreeCluster::BroadPhaseEmbreeCluster(PDEStrategy& local_pde, common:
 }
 
 void BroadPhaseEmbreeCluster::stepRecursive(Delta2::collision::Cluster& cluster, bool first_call) {
+    printf("Step size: %f\n", cluster.step_size);
     double start_step = cluster.step_size;
     bool success = false;
+    // for (Particle* p : cluster.particles) {
+    //     if (!p->is_static) {
+    //         printf("stepRecursive  pre: %i, last time: %f, current time: %f\n", p->id, p->last_state.getTime(), p->current_state.getTime());
+    //         break;
+    //     }
+    // }
     success = _local_pde.step(cluster);
+    // for (Particle* p : cluster.particles) {
+    //     if (!p->is_static) {
+    //         printf("stepRecursive post: %i, last time: %f, current time: %f\n", p->id, p->last_state.getTime(), p->current_state.getTime());
+    //         break;
+    //     }
+    // }
     if (!success) {
-        double extra_time = 0.0;
-        if (first_call) {
-            for (Particle* p : cluster.particles) {
-                if (!p->is_static) {
-                    extra_time = p->current_state.getTime() - p->last_state.getTime();
-                    p->current_state = p->current_state.interpolate(p->last_state, (p->last_state.getTime() + p->current_state.getTime()) / 2.0);
-                    cluster.min_current_time = std::min(cluster.min_current_time, p->current_state.getTime());
-                    assert(p->last_state.getTime() <= cluster.min_current_time);
-                }
+        printf("Fail\n");
+        // Interpolate current_new back to 50% between last_start and current_start
+        // Set future_new to current_start
+        // Step forward from there to the current_start (recursive call)
+        // Store current_new (to use in last_final)
+        // Set timestep size to step_size_start / 2.0 and project new future state
+        // Step forward (recursive call)
+        // Step forward (recursive call)
+        // Set last_final to the stored current_new and each of the particles last_time_step_size to min(last_time_step_size, step_size_start / 2.0)
+        
+        double new_min_time = 0.0;
+        for (Particle* p : cluster.particles) {
+            if (!p->is_static) {
+                new_min_time = std::max(new_min_time, (p->last_state.getTime() + p->current_state.getTime()) / 2.0);
             }
         }
-        cluster.step_size = (cluster.step_size + extra_time) / (first_call ? 3.0 : 2.0);
 
-        if (cluster.step_size < 1e-6) {
-            throw std::runtime_error("Step size is too small");
+        cluster.min_current_time = new_min_time; // in the next stepRecursive call all the particles will get rolled back to this time
+        printf("Rolling back to %f\n", cluster.min_current_time);
+
+        double step_size;
+        for (Particle* p : cluster.particles) {
+            if (!p->is_static) {
+                p->future_state = p->current_state;
+                p->current_state = p->current_state.interpolate(p->last_state, new_min_time);
+                // cluster.min_current_time = std::min(cluster.min_current_time, p->current_state.getTime());
+                step_size = p->future_state.getTime() - new_min_time;
+            }
         }
 
-        // cluster.step_size *= 0.5;
-        if (first_call) {
-            stepRecursive(cluster, first_call);
+        cluster.step_size = step_size;
+        stepRecursive(cluster, false);
+
+        std::vector<State> last_state_final;
+        for (Particle* p : cluster.particles) {
+            if (!p->is_static) {
+                last_state_final.push_back(p->current_state);
+            }
+        }
+
+        cluster.step_size = start_step / 2.0;
+
+        for (Particle* p : cluster.particles) {
+            if (!p->is_static) {
+                cluster.min_current_time = p->current_state.getTime();
+                p->projectFutureState(cluster.step_size);
+            }
+        }
+
+        for (Particle* p : cluster.particles) {
+            if (!p->is_static) {
+                printf("Step 2 %i, last: %f, current: %f, future: %f\n", p->id, p->last_state.getTime(), p->current_state.getTime(), p->future_state.getTime());
+            }
         }
         stepRecursive(cluster, false);
+        for (Particle* p : cluster.particles) {
+            if (!p->is_static) {
+                cluster.min_current_time = p->current_state.getTime();
+                printf("Step 3 %i, last: %f, current: %f, future: %f\n", p->id, p->last_state.getTime(), p->current_state.getTime(), p->future_state.getTime());
+            }
+        }
         stepRecursive(cluster, false);
-        printf("Cluster step failed.  Reducing time step size to %f and trying again.\n", cluster.step_size);
+        
+        for (Particle* p : cluster.particles) {
+            if (!p->is_static) {
+                printf("Post steps %i, last: %f, current: %f, future: %f\n", p->id, p->last_state.getTime(), p->current_state.getTime(), p->future_state.getTime());
+            }
+        }
+
+        int i = 0;
+        for (Particle* p : cluster.particles) {
+            if (!p->is_static) {
+                p->last_state = last_state_final[i++];
+                p->last_time_step_size = std::min(p->last_time_step_size, start_step / 2.0);
+            }
+        }
+
+        for (Particle* p : cluster.particles) {
+            if (!p->is_static) {
+                printf("End %i, last: %f, current: %f, future: %f\n", p->id, p->last_state.getTime(), p->current_state.getTime(), p->future_state.getTime());
+            }
+        }
+        
     }
+    else {
+        printf("Success\n");
+    }
+
     cluster.step_size = start_step;
 }
 
@@ -150,9 +226,30 @@ void BroadPhaseEmbreeCluster::step(model::ParticleHandler& particles) {
             // printf("%i (sleeping) stepped forward %f\n", clusters[cluster_i].step_size, cluster_i);
         }
         else {
+            printf("Advancing from %f to %f (%f)\n", clusters[cluster_i].min_current_time, clusters[cluster_i].min_current_time + clusters[cluster_i].step_size, clusters[cluster_i].step_size);
             stepRecursive(clusters[cluster_i], true);
+            for (Particle* p : clusters[cluster_i].particles) {
+                if (!p->is_static) {
+                    printf("Post step %i, last: %f, %f\n", p->id, p->last_state.getTime(), p->current_state.getTime());
+                    break;
+                }
+            }
 
             // printf("%i stepped forward %f\n", cluster_i, clusters[cluster_i].step_size);
+        }
+    }
+
+    double min_current = 100000.0;
+    for (Particle* p : particles) {
+        if (!p->is_static) {
+            assert(p->last_state.getTime() <= p->current_state.getTime() || p->current_state.getTime() == 0 || p->is_static);
+            min_current = std::min(min_current, p->current_state.getTime());
+            printf("Particle %i last: %f, current: %f\n", p->id, p->last_state.getTime(), p->current_state.getTime());
+        }
+    }
+    for (Particle* p : particles) {
+        if (!p->is_static) {
+           assert(p->last_state.getTime() <= min_current);    
         }
     }
 
