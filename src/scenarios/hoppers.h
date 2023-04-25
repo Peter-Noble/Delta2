19ac06feb4a38eff1a99089ffccbea3b402001dd
Peter-Noble <peter@noblesque.org.uk>
2023-04-25 23:52:40 +0100

    Updated hopper scenario

--- src/scenarios/hoppers.h
+++ src/scenarios/hoppers.h
@@ -11,7 +11,7 @@ namespace Delta2 {
         void hoppers(std::vector<Delta2::Particle>& particles, strategy::ContactForceStrategy& force_strategy) {
             Eigen::MatrixXd V;
             Eigen::MatrixXi F;
-            Delta2::common::plane(100, V, F);
+            Delta2::common::plane(26, V, F);
             std::shared_ptr<Delta2::MeshData> P(new Delta2::MeshData(V, F, globals::opt, true));
             std::shared_ptr<Delta2::MeshData> H(new Delta2::MeshData(std::string(std::getenv("MESH_DIR")) + "/hopper.obj", globals::opt));
             
@@ -63,45 +63,49 @@ namespace Delta2 {
                 size = globals::opt.scenario_size;
             }
 
+            Delta2::common::cube(0.1, V, F);
+            std::shared_ptr<Delta2::MeshData> C(new Delta2::MeshData(V, F, globals::opt));
+
             for (int x = 0; x < size; x++) {
                 for (int y = 0; y < size; y++) {
                     Eigen::Vector3d centre = {24.6 * x, 24.6 * y, 0};
                     {
                         auto& p = particles.emplace_back(P, 1.0, 1.0, 0.25);
-                        p.current_state.setTranslation(centre + Eigen::Vector3d({0, 0, -5}));
+                        p.current_state.setTranslation(centre + Eigen::Vector3d({0, 0, -15}));
                         p.is_static = true;
                     }
                     {
                         auto& p = particles.emplace_back(H, 1.0, 1.0, 0.25);
+                        p.current_state.setRotation(common::eulerAnglesToQuaternion(Eigen::Vector3d({common::degToRad(90.0), 0, 0})));
                         p.current_state.setTranslation(centre);
                         p.is_static = true;
                     }
-                    for (int px = 0; px < 3; px++) {
-                        for (int py = 0; py < 3; py++) {
+                    for (int px = -2; px < 3; px++) {
+                        for (int py = -2; py < 3; py++) {
                             for (int pz = 0; pz < 2; pz++) {
-                                auto& p = particles.emplace_back(large[Delta2::globals::opt.rand(large.size())], 1.0, 1.0, 0.25);
-                                p.current_state.setTranslation()
+                                if ((px + py + pz) % 2 != 0) {
+                                    auto& p = particles.emplace_back(large[Delta2::globals::opt.rand(large.size())], 1.0, 1.0, 0.25);
+                                    // auto& p = particles.emplace_back(C, 1.0, 1.0, 0.25);
+                                    p.current_state.setTranslation(centre + Eigen::Vector3d({px * 3.0, py * 3.0, pz * 3}));
+                                }
                             }
-                            for (int pz = 0; pz < 4; pz++) {
-                                
+                            for (int pz = 0; pz < 3; pz++) {
+                                auto& p = particles.emplace_back(medium[Delta2::globals::opt.rand(medium.size())], 1.0, 1.0, 0.25);
+                                // auto& p = particles.emplace_back(C, 1.0, 1.0, 0.25);
+                                p.current_state.setTranslation(centre + Eigen::Vector3d({px * 5, py * 5, 10 + pz * 3}));
                             }
                             for (int pz = 0; pz < 6; pz++) {
-                                
+                                auto& p = particles.emplace_back(small[Delta2::globals::opt.rand(small.size())], 1.0, 1.0, 0.25);
+                                // auto& p = particles.emplace_back(C, 1.0, 1.0, 0.25);
+                                p.current_state.setTranslation(centre + Eigen::Vector3d({px * 4.0, py * 4.0, 24 + pz * 3}));
                             }
                         }
                     }
+
+                    // auto& p = particles.emplace_back(C, 1.0, 1.0, 0.25);
+                    // p.current_state.setTranslation(centre + Eigen::Vector3d({6.0, 4.0, -2}));
+                    // p.current_state.setVelocity(Eigen::Vector3d({0, 0, -10}));
                 }
-            }
-
-
-            {
-                Delta2::common::cube(1.0, V, F);
-                std::shared_ptr<Delta2::MeshData> M(new Delta2::MeshData(V, F, globals::opt));
-                auto& p = particles.emplace_back(M, 1.0, 1.0, 0.1);
-                p.current_state.setVelocity({Delta2::globals::opt.rand_float(3), Delta2::globals::opt.rand_float(0.5), Delta2::globals::opt.rand_float(0.5)});
-                p.current_state.setTranslation({0, 0, 1.3 + Delta2::globals::opt.rand_float(0.5)});
-                p.current_state.setRotation(Delta2::common::eulerAnglesToQuaternion(Eigen::Vector3d({Delta2::common::degToRad(180 - Delta2::globals::opt.rand_float(360)), Delta2::common::degToRad(180 - Delta2::globals::opt.rand_float(360)), Delta2::common::degToRad(180 - Delta2::globals::opt.rand_float(360))})));
-                p.current_state.setAngular({5-Delta2::globals::opt.rand_float(10), 5-Delta2::globals::opt.rand_float(10), 5-Delta2::globals::opt.rand_float(10)});
             }
         }
     }
